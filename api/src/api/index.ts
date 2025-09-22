fe1381adc62262cae287b6589b56624d56576140
Jaiswal-Shubham <shkjais@gmail.com>
2025-09-23 01:15:34 +0530

    deployemnt config changes

--- api/src/api/index.ts
+++ api/src/api/index.ts
@@ -1,5 +1,5 @@
 // backend/src/api/index.ts
-import 'dotenv/config'; 
+import 'dotenv/config';
 import express, { Express } from 'express';
 import cors from 'cors';
 import config from '../config';
@@ -8,25 +8,15 @@ import errorHandler from '../middleware/errorHandler';
 
 const app: Express = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// API Routes
+
 app.use('/api', apiRoutes);
 
-// Global Error Handler
+
 app.use(errorHandler);
 
-
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(config.port, () => {
-    console.log(`Server is running on http://localhost:${config.port}`);
-  });
-}
-
-// Export for Vercel
 export default app;
-
